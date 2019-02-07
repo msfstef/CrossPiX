08588d4cc54a3c2bc1b33672ce534c4eb5a91c3a
Stefanos Mousafeiris <sm6114@imperial.ac.uk>
2019-02-07 20:56:06 +0200

    Updates

--- precache-manifest.d0bed361cb8aab488e8bb4624e3a4349.js
+++ precache-manifest.d0bed361cb8aab488e8bb4624e3a4349.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/cross_stitch_generator/static/js/runtime~main.a64a8c8d.js"
   },
   {
-    "revision": "e8626890f87c45af364a",
-    "url": "/cross_stitch_generator/static/js/main.e8626890.chunk.js"
+    "revision": "27ba049322e1c05b602f",
+    "url": "/cross_stitch_generator/static/js/main.27ba0493.chunk.js"
   },
   {
     "revision": "d1cf2b5a0185e68c75a0",
     "url": "/cross_stitch_generator/static/js/1.d1cf2b5a.chunk.js"
   },
   {
-    "revision": "e8626890f87c45af364a",
+    "revision": "27ba049322e1c05b602f",
     "url": "/cross_stitch_generator/static/css/main.5c928e67.chunk.css"
   },
   {
-    "revision": "c4c803e552b90be51ae3682da3a05c71",
+    "revision": "295994b67b7630ff1c525913af6a497f",
     "url": "/cross_stitch_generator/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cross_stitch_generator/precache-manifest.4917df7d93dba5d6443353b9fe4fc96d.js"
+  "/cross_stitch_generator/precache-manifest.d0bed361cb8aab488e8bb4624e3a4349.js"
 );
 
 workbox.clientsClaim();
